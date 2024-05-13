a6c0864896c8ace6e1b6e9c0e7bf4acadeb8d25f
AmourAnurag <anurag875636@gmail.com>
2024-05-14 00:39:53 +0530

    skills change

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const languages = [
+  "fa-brands fa-bootstrap",
+  "fa-brands fa-git-alt",
+  "fa-brands fa-css3-alt",
+  "fa-brands fa-react",
+  "fa-brands fa-square-js",
+  "fa-brands fa-java",
+  "fa-brands fa-html5",
+];
+console.log(languages);
+let container = document.getElementById("skill-container");
+let html = ""; // Initialize an empty string to store HTML content
+for (let index = 0; index < languages.length; index++) {
+  // Concatenate HTML for each div in each iteration
+  html += `<div class="languages scroll-bottom">
+    <i class='${languages[index]}'></i>
+  </div>`;
+}
+container.innerHTML = html; 
+
+// languages.map(url=>container.innerHTML=`<div class="languages">
+// <i class='${url}'></i>
+
+// </div>`)
+// // container.innerHTML = `<div class="languages">
+// <i class='${languages[0]}'></i>
+// </div> <div class="languages">
+// <i class='${languages[0]}'></i>
+// </div>`;
+
 let words = document.querySelectorAll(".word");
 
 words.forEach((word) => {
@@ -39,92 +69,88 @@ changeText();
 setInterval(changeText, 4000);
 
 // ..................................CIRCLE SKILLS .............................
-const circles = document.querySelectorAll('.circle')
-circles.forEach(elem=>{
-  var dots=elem.getAttribute('data-dots');
-  var marked=elem.getAttribute('data-percent');
-  var percent=Math.floor(dots*marked/100);
+const circles = document.querySelectorAll(".circle");
+circles.forEach((elem) => {
+  var dots = elem.getAttribute("data-dots");
+  var marked = elem.getAttribute("data-percent");
+  var percent = Math.floor((dots * marked) / 100);
   var points = "";
-  var rotate =360/dots;
+  var rotate = 360 / dots;
   for (let i = 0; i < dots; i++) {
-     points+=`<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
   }
-  elem.innerHTML=points
+  elem.innerHTML = points;
 
-  const pointsMarked=elem.querySelectorAll('.points');
+  const pointsMarked = elem.querySelectorAll(".points");
   for (let i = 0; i < percent; i++) {
-   pointsMarked[i].classList.add('marked')
+    pointsMarked[i].classList.add("marked");
   }
-})
+});
 
 // mix it up portfolio site
-var mixer = mixitup('.portfolio-gallery');
-
-
+var mixer = mixitup(".portfolio-gallery");
 
 //active menu /////////////////////////////////////
-let menuLi=document.querySelectorAll('header ul li a');
-let section=document.querySelectorAll('section');
-function activeMenu(){
-  let len=section.length;
-  while (--len && window.scrollY +97 < section[len].offsetTop) {
-    
-  }
-  menuLi.forEach(sec => sec.classList.remove("active"));
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+function activeMenu() {
+  let len = section.length;
+  while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+  menuLi.forEach((sec) => sec.classList.remove("active"));
   menuLi[len].classList.add("active");
 }
 activeMenu();
-window.addEventListener("scroll",activeMenu)
+window.addEventListener("scroll", activeMenu);
 
 //sticy navbar///////////////////
-const header=document.querySelector("header");
-window.addEventListener("scroll",function() {
-  header.classList.toggle("sticky",window.scrollY>50)
-})
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 50);
+});
 
 //toggle icon navbar
-let menuIcon=document.querySelector("#menu-icon")
-let menu=document.querySelector(".fa-bars")
-let navlist=document.querySelector(".navlist")
-let count=1;
-menuIcon.onclick=()=>{
+let menuIcon = document.querySelector("#menu-icon");
+let menu = document.querySelector(".fa-bars");
+let navlist = document.querySelector(".navlist");
+let count = 1;
+menuIcon.onclick = () => {
   // menuIcon.classList.toggle("fa-x")
   // menu.classList.remove("fa-bars")
   if (count) {
-    menuIcon.style.transform="rotate(-180deg)"
+    menuIcon.style.transform = "rotate(-180deg)";
     count--;
   } else {
-    menuIcon.style.transform="rotate(180deg)"
-    menuIcon.style.transition="2s"
-   count++;
+    menuIcon.style.transform = "rotate(180deg)";
+    menuIcon.style.transition = "2s";
+    count++;
   }
-  menu.classList.toggle("fa-x")
-  navlist.classList.toggle("open")
-}
+  menu.classList.toggle("fa-x");
+  navlist.classList.toggle("open");
+};
 
-window.onscroll=()=>{
-  menu.classList.remove('fa-x');
-  navlist.classList.remove('open')
-}
+window.onscroll = () => {
+  menu.classList.remove("fa-x");
+  navlist.classList.remove("open");
+};
 
 //parallax //////////////////
-const observer=new IntersectionObserver((entries)=>{
-  entries.forEach((entry)=>{
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("show-items")
+      entry.target.classList.add("show-items");
     } else {
-      entry.target.classList.remove("show-items")
+      entry.target.classList.remove("show-items");
     }
-   })
-})
-const scrollScale=document.querySelectorAll(".scroll-scale");
-scrollScale.forEach((el)=>observer.observe(el))
+  });
+});
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el) => observer.observe(el));
 
-const scrollBottom=document.querySelectorAll(".scroll-bottom");
-scrollBottom.forEach((el)=>observer.observe(el))
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el) => observer.observe(el));
 
-const scrollTop=document.querySelectorAll(".scroll-top");
-scrollTop.forEach((el)=>observer.observe(el))
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el) => observer.observe(el));
 
-const scrollright=document.querySelectorAll(".scroll-right");
-scrollright.forEach((el)=>observer.observe(el))
+const scrollright = document.querySelectorAll(".scroll-right");
+scrollright.forEach((el) => observer.observe(el));
